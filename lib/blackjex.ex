67cab407216dcefaa5104a0be499098f3d7e348f
sash stasyk <sashman90@gmail.com>
2020-11-20 11:00:36 +0000

    Initial project generation

---
+++ lib/blackjex.ex
@@ -0,0 +1,18 @@
+defmodule Blackjex do
+  @moduledoc """
+  Documentation for `Blackjex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Blackjex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/blackjex_test.exs
@@ -0,0 +1,8 @@
+defmodule BlackjexTest do
+  use ExUnit.Case
+  doctest Blackjex
+
+  test "greets the world" do
+    assert Blackjex.hello() == :world
+  end
+end
