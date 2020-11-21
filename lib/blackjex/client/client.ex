ae853a96596027428965daaba182df287621ee1a
sash stasyk <sashman90@gmail.com>
2020-11-21 21:38:17 +0000

    Initial WIP client over network

--- lib/blackjex/client/client.ex
+++ lib/blackjex/client/client.ex
@@ -1,2 +1,13 @@
 defmodule Blackjex.Client.Client do
+  def connet(sid, cookie) do
+    {:ok, _} = Node.start(:client)
+
+    sid = String.to_atom(sid)
+    Node.set_cookie(String.to_atom(cookie))
+    Node.connect(sid)
+  end
+
+  def call(sid, command) do
+    GenServer.call({Blackjex.Server.Server, sid}, command)
+  end
 end
--- lib/mix/tasks/play.ex
+++ lib/mix/tasks/play.ex
@@ -1,52 +1,12 @@
 defmodule Mix.Tasks.Play do
   use Mix.Task
+  alias Blackjex.Client.{Client, Prompt}
 
   def run([sid, cookie]) do
     IO.puts("Welcome to Blackjex Blackjack!")
-    {:ok, _} = Node.start(:client)
+    Client.connet(sid, cookie)
+    Prompt.join_game(sid)
 
-    sid = String.to_atom(sid)
-    Node.set_cookie(String.to_atom(cookie))
-    Node.connect(sid)
-
-    name = ExPrompt.string("What is your name? ")
-
-    call(sid, {:join, name})
-    |> IO.inspect()
-
-    start_loop(sid)
-  end
-
-  defp start_loop(sid) do
-    ask_for_input(sid)
-  end
-
-  defp ask_for_input(sid) do
-    options = [
-      {
-        "hit",
-        :hit
-      },
-      {
-        "stick",
-        :stick
-      },
-      {
-        "cards",
-        :cards
-      }
-    ]
-
-    answer = ExPrompt.choose("What is you move? ", options |> Enum.map(&(&1 |> elem(0))))
-    answer = options |> Enum.at(answer) |> elem(1)
-
-    call(sid, {answer})
-    |> IO.inspect()
-
-    ask_for_input(sid)
-  end
-
-  defp call(sid, command) do
-    GenServer.call({Blackjex.Server.Server, sid}, command)
+    Prompt.play_game(sid)
   end
 end
